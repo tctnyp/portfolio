937e69da1a9c98faab591c8746510d7237933971
alphaTCT3209 <alphatct3209@hotmail.com>
2026-07-27 17:05:08 +0800

    Update landing page

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-// Highlight current page [NAVBAR]
+// Highlight current page in navbar
 
 const currentPage = window.location.pathname.split("/").pop() || "index.html";
 
 document.querySelectorAll(".nav-links a").forEach(link => {
-    link.classList.remove("active");
 
     if (link.getAttribute("href") === currentPage) {
         link.classList.add("active");
+    } else {
+        link.classList.remove("active");
     }
+
 });
