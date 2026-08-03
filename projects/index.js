a73fe92f729407c69eaa2a0051a07f39f0d3c9f0
alphaTCT3209 <alphatct3209@hotmail.com>
2026-08-03 15:37:30 +0800

    Fixed /projects error, and purged old .html files in root

---
+++ projects/index.js
@@ -0,0 +1,10 @@
+function highlightCurrentPage() {
+    const currentPath = new URL(window.location.href).pathname.replace(/index\.html$/, "").replace(/\/+$/, "/");
+
+    document.querySelectorAll(".nav-links a").forEach((link) => {
+        const linkPath = new URL(link.href, window.location.href).pathname.replace(/index\.html$/, "").replace(/\/+$/, "/");
+        link.classList.toggle("active", linkPath === currentPath);
+    });
+}
+
+highlightCurrentPage();
